46e4cd3ed61710dff1204d0acb9f87c5473ebaa6
Tisal-Thenuwara <Tisal396@gmail.com>
2021-11-15 17:32:11 +0530

    Added comments

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
+//Creates a scene
 const scene = new THREE.Scene();
+//Creates a camera
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
+//Adds it to our page
 document.body.appendChild( renderer.domElement );
 
+//Adds a cube to the scene
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
@@ -12,4 +16,11 @@ scene.add( cube );
 
 camera.position.z = 5;
 
-renderer.render(scene, camera);
+//renderer.render(scene, camera);
+//This adds the cube manually
+
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+animate();
