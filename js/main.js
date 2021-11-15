e99d42b3477ab9f7ae87841250fa0458e182fffa
Tisal Thenuwara <tisal396@gmail.com>
2021-11-15 21:58:05 +0530

    Added player class

--- js/main.js
+++ js/main.js
@@ -79,6 +79,17 @@ function createTrack()
 
 createTrack();
 
+class player
+{
+    constructor()
+    {
+        const geometry = new THREE.SphereGeometry( 15, 32, 16 );
+        const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+        const sphere = new THREE.Mesh( geometry, material );
+        scene.add( sphere );
+    }
+}
+
 //creates new doll
 let doll = new Doll();
 
