1ec4afe9b74ce5b6aaa3a816e430e7b93aa1a134
Tisal Thenuwara <tisal396@gmail.com>
2021-11-17 12:53:03 +0530

    Line spacing

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 //Creates a scene
 const scene = new THREE.Scene();
+
 //Creates a camera
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
+
 //Adds it to our page
 document.body.appendChild( renderer.domElement );
 
